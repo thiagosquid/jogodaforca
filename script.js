e07e33dd449617976f7d0a7757d18729130f2708
Thiago Gomes <thiagofg.eng@gmail.com>
2021-07-17 13:37:10 -0300

    Chances e outras melhorias
    Inclusão do número de chances e outras melhorias

--- script.js
+++ script.js
@@ -8,26 +8,22 @@ let letreiro = document.getElementById('impr');
 let inicia = document.getElementById('btInicia');
 let letra='';
 let letrasEscolhidas = [];
+let chancesBox = document.getElementById('chances');
 
 inicia.addEventListener('click', recebePalavra);
 
 let botoes = document.body.querySelectorAll(".word");
 
 // Pra gerar eventListener de todas as letras
-
 for(let x=0; x<botoes.length; x++){
    botoes[x].addEventListener("click", function(){
     letra = this.value;
         if(letrasEscolhidas.indexOf(letra) == -1){
-            if(chances >= 0){
+            if(chances > 0){
             verificaLetra(letra);
-            }else{
-                alert('Você perdeu');
-                if(confirm('Deseja jogar novamente?')){
-                    recebePalavra();
-                }
             }
-        }else{alert('Letra já escolhida, digite outra')}
+        }else{alert('Letra já escolhida, digite outra')
+        }
    });
 }
     //Inicialização do jogo
@@ -35,6 +31,7 @@ function recebePalavra(){
     letrasEscolhidas = [];
     let impr = '';
     chances = 5;
+    chancesBox.innerHTML = chances;
     palavraSel = Math.floor(Math.random()*lista.length);
     tamPal = lista[palavraSel].length;
     palavraOculta = lista[palavraSel];
@@ -63,10 +60,18 @@ function verificaLetra(l){
                 espacos[cont] = l;
             }
         }
-    if(palavraOculta.indexOf(l) == -1){
-        chances -= 1;
-    }
     alteraImpressao();
+        if(palavraOculta.indexOf(l) == -1){
+        chances -= 1;
+        chancesBox.innerHTML = chances;
+            if(chances == 0){
+                alert('Você perdeu');
+                if(confirm('Deseja jogar novamente?')){
+                    recebePalavra();
+                }
+            }
+    }
+    
 }
 
 function alteraImpressao(){
